52815abb3db565c778023bca5e4f951697a167cb
Gudz Danil <gudz.danil@gmail.com>
2015-03-24 03:58:28 +0200

    connected answers with questions

--- js/QuestionsFactory.js
+++ js/QuestionsFactory.js
@@ -1,50 +1,7 @@
 (function(win){
     function QuestionsFactory(){
-        var arr = [
-            new Answer("Для своих детей"),
-            new Answer("Заработать"),
-            new Answer("Найти работу"),
-            new Answer("Есть идея для стартапа!"),
-            new Answer("Я не знаю, посоветуй мне язык"),
-            new Answer("Просто так"),
-            new Answer("Мне интересно"),
-            new Answer("Проверить себя"),
-            new Answer("Да"),//придумали отличную идею?
-            new Answer("Нет. Просто хочу начать"),
-            new Answer("По-быстрому"),
-            new Answer("Правильно"),
-            new Answer("Немного попотеть"),
-            new Answer("Авто"),
-            new Answer("Мех."),
-            new Answer("Самым сложным путем"),
-            new Answer("Lego"),
-            new Answer("Пластилин"),
-            new Answer("У меня старая и уродливая игрушка, но я ее обожаю!"),
-            new Answer("Да"),//менее надежное
-            new Answer("Нет"),//менее надежное
-            new Answer("Не уверен"),//Менее надежное
-            new Answer("Нет"),//как твиттер
-            new Answer("WEB"),//под какую платформу
-            new Answer("Энтерпрайз"),
-            new Answer("Мобильные"),
-            new Answer("3D/игры"),
-            new Answer("iOS"),
-            new Answer("Android"),
-            new Answer("Стартапе"),
-            new Answer("Корпорации"),
-            new Answer("Фанат!"),
-            new Answer("Ну норм..."),
-            new Answer("Отстой"),
-            new Answer("Мне все равно, просто хочу $$$"),
-            new Answer("WEB"),//Платформа/область
-            new Answer("Front-end"),
-            new Answer("Back-end"),
-            new Answer("Apple"),
-            new Answer("Microsoft"),
-            new Answer("Facebook"),
-            new Answer("Google")
-        ];
     }
+    QuestionsFactory.prototype = Object.create(Factory.prototype);
 
     win.QuestionsFactory = new QuestionsFactory();
 })(window);
--- js/script.js
+++ js/script.js
@@ -1,140 +1,180 @@
-(function(){
-    var results = {
-        'py': 'Python',
-        'cs': 'C#',
-        'oc': 'Objective C',
-        'ja': 'Java',
-        'js': 'JavaScript',
-        'ru': 'Ruby',
-        'php': 'PHP',
-        'c': 'C',
-        'cpp': 'C++'
-    };
-    var questions = [
-     
-        new Question(1, "Почему вы решили изучать программирование?"),
+(function () {
 
-        //"Для своих детей"
-            //Начните с основ и двигайтесь к results.py
+    var AnswersFactory = new Factory('Answer');
+    var QuestionsFactory = new Factory('Question');
+    var ResultsFactory = new Factory('String');
 
-        //"Заработать"
-        new Question(2, "Что дальше?"),
-            //"Найти работу"
-            new Question(3, "Платформа/область?"),
-                //"Хочу работать в большой технической компании"
-                new Question(4, "Какая компания Вам больше симпатизирует?"),
-                    //"Facebook"
-                        //results.py
+    ResultsFactory.push(getResArr());
 
-                    // "Windows"
-                        //results.cs
+    AnswersFactory.push(getAnswArr());
 
-                    // "Google"
-                        //results.py
+    QuestionsFactory.push(getQuestArr());
 
-                    // "Apple"
-                        //results.oc
-
-                // "Мне все равно просто хочу $$$"
-                    //results.ja
-
-                // "Web"
-                new Question(5, "Какая часть Веба вам больше нравится?"),
-                    // "Front-end"
-                        //results.js
-
-                    // "Back-end"
-                    new Question(6, "Хочу работать в ..."),
-                        //Стартапе
-                        new Question(7, "Хотите попробовать что-то новое с большим потенциалом но менее надежное?"),
-                            //Да
-                                //results.js
-
-                            //Нет
-                            new Question(8, "Ваша любимая игрушка?"),
-                                //Lego
-                                    //results.py
-                                //Пластилин
-                                    //results.ru
-                                //У меня старая и уродливая игрушка, но я ее обожаю
-                                    //results.php
-
-                            //Не уверен
-                            new Question(8),
+    connectQuestAnsw();
 
 
-                        //Корпорации
-                        new Question(9, "Как Вы относитесь к Microsoft?"),
-                        // "Фанат"
-                            //results.cs
+    function connectQuestAnsw() {
+        QuestionsFactory.findByVal("Почему вы решили изучать программирование?").pushAnswers([
+            AnswersFactory.findByVal("Для своих детей"),
+            AnswersFactory.findByVal("Заработать"),
+            AnswersFactory.findByVal("Просто так"),
+            AnswersFactory.findByVal("Мне интересно"),
+            AnswersFactory.findByVal("Проверить себя"),
+            AnswersFactory.findByVal("Я не знаю, посоветуй мне язык")
+        ]);
+        QuestionsFactory.findByVal("Что дальше?").pushAnswers([
+            AnswersFactory.findByVal("Найти работу"),
+            AnswersFactory.findByVal("Есть идея для стартапа!")
+        ]);
+        QuestionsFactory.findByVal("Платформа/область?").pushAnswers([
+            AnswersFactory.findByVal("Хочу работать в большой технической компании"),
+            AnswersFactory.findByVal("Мне все равно, просто хочу $$$"),
+            AnswersFactory.findById('platArea'),
+            AnswersFactory.findByVal("Энтерпрайз"),
+            AnswersFactory.findByVal("Мобильные"),
+            AnswersFactory.findByVal("3D/игры")
+        ]);
+        QuestionsFactory.findByVal("Какая компания Вам больше симпатизирует?").pushAnswers([
+            AnswersFactory.findByVal("Apple"),
+            AnswersFactory.findByVal("Microsoft"),
+            AnswersFactory.findByVal("Facebook"),
+            AnswersFactory.findByVal("Google")
+        ]);
+        QuestionsFactory.findByVal("Какая часть Веба вам больше нравится?").pushAnswers([
+            AnswersFactory.findByVal("Front-end"),
+            AnswersFactory.findByVal("Back-end")
+        ]);
+        QuestionsFactory.findByVal("Хочу работать в ...").pushAnswers([
+            AnswersFactory.findByVal("Стартапе"),
+            AnswersFactory.findByVal("Корпорации")
+        ]);
+        QuestionsFactory.findByVal("Хотите попробовать что-то новое с большим потенциалом но менее надежное?").pushAnswers([
+            AnswersFactory.findById('secureY'),
+            AnswersFactory.findById('secureN'),
+            AnswersFactory.findById('secureYN')
+        ]);
+        QuestionsFactory.findByVal("Ваша любимая игрушка?").pushAnswers([
+            AnswersFactory.findByVal("Lego"),
+            AnswersFactory.findByVal("Пластилин"),
+            AnswersFactory.findByVal("У меня старая и уродливая игрушка, но я ее обожаю!")
+        ]);
+        QuestionsFactory.findByVal("Как Вы относитесь к Microsoft?").pushAnswers([
+            AnswersFactory.findByVal("Фанат!"),
+            AnswersFactory.findByVal("Ну норм..."),
+            AnswersFactory.findByVal("Отстой")
+        ]);
+        QuestionsFactory.findByVal("Какая ОС?").pushAnswers([
+            AnswersFactory.findByVal("iOS"),
+            AnswersFactory.findByVal("Android")
+        ]);
+        QuestionsFactory.findByVal("Под какую платформу?").pushAnswers([
+            AnswersFactory.findByVal("Энтерпрайз"),
+            AnswersFactory.findByVal("Мобильные"),
+            AnswersFactory.findByVal("3D/игры"),
+            AnswersFactory.findById('platf')
+        ]);
+        QuestionsFactory.findByVal("Ваше приложение работает с информацией в реальном времени, как Twitter?").pushAnswers([
+            AnswersFactory.findById('twit'),
+            AnswersFactory.findById('secureYN')
+        ]);
+        QuestionsFactory.findByVal("Придумали отличную идею?").pushAnswers([
+            AnswersFactory.findById('idea'),
+            AnswersFactory.findByVal("Нет. Просто хочу начать")
+        ]);
+        QuestionsFactory.findByVal("Предпочитаю изучать новое:").pushAnswers([
+            AnswersFactory.findByVal("По-быстрому"),
+            AnswersFactory.findByVal("Правильно"),
+            AnswersFactory.findByVal("Немного попотеть"),
+            AnswersFactory.findByVal("Самым сложным путем")
+        ]);
+        QuestionsFactory.findByVal("Авто или механическая КПП?").pushAnswers([
+            AnswersFactory.findByVal("Авто"),
+            AnswersFactory.findByVal("Мех.")
+        ]);
 
-                        // "Ну норм",
-                            //results.ja
+    }
 
-                        // "Отстой"
-                            //results.ja
+    function getResArr() {
+        return [
+            {id: 'py', val: 'Python'},
+            {id: 'cs', val: 'C#'},
+            {id: 'oc', val: 'ObjectiveC'},
+            {id: 'ja', val: 'Java'},
+            {id: 'js', val: 'JavaScript'},
+            {id: 'ru', val: 'Ruby'},
+            {id: 'php', val: 'PHP'},
+            {id: 'c', val: 'C'},
+            {id: 'cpp', val: 'C++'}
+        ];
+    }
 
-                // "Ентерпрайз"
-                new Question(9),
+    function getAnswArr() {
+        return [
+            "Для своих детей",
+            "Заработать",
+            "Найти работу",
+            "Есть идея для стартапа!",
+            "Я не знаю, посоветуй мне язык",
+            "Просто так",
+            "Мне интересно",
+            "Проверить себя",
+            "Хочу работать в большой технической компании",
+            {id: 'idea', val: "Да"},
+            "Нет. Просто хочу начать",
+            "По-быстрому",
+            "Правильно",
+            "Немного попотеть",
+            "Авто",
+            "Мех.",
+            "Самым сложным путем",
+            "Lego",
+            "Пластилин",
+            "У меня старая и уродливая игрушка, но я ее обожаю!",
+            {id: 'secureY', val: "Да"},
+            {id: 'secureN', val: "Нет"},
+            {id: 'secureYN', val: "Не уверен"},
+            {id: 'twit', val: "Нет"},
+            {id: 'platf', val: "WEB"},
+            "Энтерпрайз",
+            "Мобильные",
+            "3D/игры",
+            "iOS",
+            "Android",
+            "Стартапе",
+            "Корпорации",
+            "Фанат!",
+            "Ну норм...",
+            "Отстой",
+            "Мне все равно, просто хочу $$$",
+            {id: 'platArea', val: "WEB"},
+            "Front-end",
+            "Back-end",
+            "Apple",
+            "Microsoft",
+            "Facebook",
+            "Google"
+        ];
+    }
 
-                // "Мобильные"
-                new Question(10, "Какая ОС?"),
-                    // "iOS"
-                        //results.oc
+    function getQuestArr() {
+        return [
+            "Почему вы решили изучать программирование?",
+            "Что дальше?",
+            "Платформа/область?",
+            "Какая компания Вам больше симпатизирует?",
+            "Какая часть Веба вам больше нравится?",
+            "Хочу работать в ...",
+            "Хотите попробовать что-то новое с большим потенциалом но менее надежное?",
+            "Ваша любимая игрушка?",
+            "Как Вы относитесь к Microsoft?",
+            "Какая ОС?",
+            "Под какую платформу?",
+            "Ваше приложение работает с информацией в реальном времени, как Twitter?",
+            "Придумали отличную идею?",
+            "Предпочитаю изучать новое:",
+            "Авто или механическая КПП?"
+        ];
+    }
 
-                    // "Android"
-                        //results.ja
-
-                // "3D игры"
-                    //results.cpp
-
-
-            //"Есть идея для стартапа"
-            new Question(11, "Под какую платформу?"),
-                //3d игры
-                    //results.cpp
-
-                //мобильные
-                new Question(10),
-
-                //ентерпрайз
-                new Question(9),
-
-                //веб
-                    new Question(12, "Ваше приложение работает с информацией в реальном времени, как Twitter?"),
-                        //да
-                            //results.js
-
-                        //нет
-                        new Question(7),
-
-        //Я не знаю, просто посоветуйте мне язык
-            //results.py
-
-        //Просто так
-        //Мне интересно
-        //Проверить себя
-            new Question(13, "Придумали отличную идею?"),
-                //да
-                new Question(11),
-
-                //нет. Просто хочу начать.
-                new Question(14, "Предпочитаю изучать новое:"),
-
-    //По-быстрому
-    //Правильно
-        //results.py
-
-    //Немного попотеть
-    new Question(15, "Авто или механическая КПП?")
-        //Авто
-            //results.ja
-        //Мех.
-            //results.c
-
-    //Самым сложным путем
-        //results.cpp
-
-    ];
-
-})();
+})
+();
